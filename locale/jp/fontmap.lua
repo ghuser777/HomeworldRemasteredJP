8d83a8d4e4001568888d261aac75f180da09703a
ghuser777 <ghuser777@users.noreply.github.com>
2015-07-10 16:41:09 +0900

    初回コミット

---
+++ locale/jp/fontmap.lua
@@ -0,0 +1,165 @@
+-- Mappings from font names to font files for different resolutions for a given language
+-- English
+
+FontMap640 =
+{
+    default = "data:UI\\Font\\MigMix2p.rcf",
+
+	BlenderFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --Subtitle and SP game fonts
+    SPSubtitleFont = "data:UI\\font\\MigMix2p.rcf",
+    LocationCardFont = "data:UI\\Font\\MigMix2p.rcf",			-- Used for mouseover tooltip
+
+    --ATI/SM fonts
+    ATIControlGroupFont = "data:UI\\Font\\MigMix2p.rcf",
+    ATISmallFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --UI fonts
+    Heading1Font = "data:UI\\Font\\MigMix2p.rcf",	-- Title font
+    Heading2Font = "data:UI\\Font\\MigMix2p.rcf",	        -- Sub title font
+    Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Window title font
+	RM_Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Subtitle font
+    Heading4Font = "data:UI\\Font\\MigMix2p.rcf",			-- Window sub title font
+    RM_ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+	ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+    ListBoxItemFont = "data:UI\\Font\\MigMix2p.rcf",
+    GenericSubtitleFont = "data:UI\\Font\\MigMix2p.rcf",
+    ChatFont       = "data:UI\\Font\\MigMix2p.rcf",			-- Font for chat text
+}
+
+FontMap800 =
+{
+    default = "data:UI\\Font\\MigMix2p.rcf",
+
+	BlenderFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --Subtitle and SP game fonts
+    SPSubtitleFont = "data:UI\\font\\MigMix2p.rcf",
+    LocationCardFont = "data:UI\\Font\\MigMix2p.rcf",			-- Used for mouseover tooltip
+
+    --ATI/SM fonts
+    ATIControlGroupFont = "data:UI\\Font\\MigMix2p.rcf",
+    ATISmallFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --UI fonts
+    Heading1Font = "data:UI\\Font\\MigMix2p.rcf",	-- Title font
+    Heading2Font = "data:UI\\Font\\MigMix2p.rcf",	        -- Sub title font
+    Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Window title font
+	RM_Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Subtitle font
+    Heading4Font = "data:UI\\Font\\MigMix2p.rcf",			-- Window sub title font
+    RM_ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+	ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+    ListBoxItemFont = "data:UI\\Font\\MigMix2p.rcf",
+    GenericSubtitleFont = "data:UI\\Font\\MigMix2p.rcf",
+    ChatFont       = "data:UI\\Font\\MigMix2p.rcf",			-- Font for chat text
+}
+
+FontMap1024 =
+{
+    default = "data:UI\\Font\\MigMix2p.rcf",
+
+	BlenderFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --Subtitle and SP game fonts
+    SPSubtitleFont = "data:UI\\font\\MigMix2p.rcf",
+    LocationCardFont = "data:UI\\Font\\MigMix2p.rcf",			-- Used for mouseover tooltip
+
+    --ATI/SM fonts
+    ATIControlGroupFont = "data:UI\\Font\\MigMix2p.rcf",
+    ATISmallFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --UI fonts
+    Heading1Font = "data:UI\\Font\\MigMix2p.rcf",	-- Title font
+    Heading2Font = "data:UI\\Font\\MigMix2p.rcf",	        -- Sub title font
+    Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Window title font
+	RM_Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Subtitle font
+    Heading4Font = "data:UI\\Font\\MigMix2p.rcf",			-- Window sub title font
+    RM_ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+	ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+    ListBoxItemFont = "data:UI\\Font\\MigMix2p.rcf",
+    GenericSubtitleFont = "data:UI\\Font\\MigMix2p.rcf",
+    ChatFont       = "data:UI\\Font\\MigMix2p.rcf",			-- Font for chat text
+}
+
+FontMap1280 =
+{
+    default = "data:UI\\Font\\MigMix2p.rcf",
+
+	BlenderFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --Subtitle and SP game fonts
+    SPSubtitleFont = "data:UI\\font\\MigMix2p.rcf",
+    LocationCardFont = "data:UI\\Font\\MigMix2p.rcf",			-- Used for mouseover tooltip
+
+    --ATI/SM fonts
+    ATIControlGroupFont = "data:UI\\Font\\MigMix2p.rcf",
+    ATISmallFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --UI fonts
+    Heading1Font = "data:UI\\Font\\MigMix2p.rcf",	-- Title font
+    Heading2Font = "data:UI\\Font\\MigMix2p.rcf",	        -- Sub title font
+    Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Window title font
+	RM_Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Subtitle font
+    Heading4Font = "data:UI\\Font\\MigMix2p.rcf",			-- Window sub title font
+    RM_ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+	ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+    ListBoxItemFont = "data:UI\\Font\\MigMix2p.rcf",
+    GenericSubtitleFont = "data:UI\\Font\\MigMix2p.rcf",
+    ChatFont       = "data:UI\\Font\\MigMix2p.rcf",			-- Font for chat text
+}
+
+FontMap1600 =
+{
+    default = "data:UI\\Font\\MigMix2p.rcf",
+
+	BlenderFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --Subtitle and SP game fonts
+    SPSubtitleFont = "data:UI\\font\\MigMix2p.rcf",
+    LocationCardFont = "data:UI\\Font\\MigMix2p.rcf",			-- Used for mouseover tooltip
+
+    --ATI/SM fonts
+    ATIControlGroupFont = "data:UI\\Font\\MigMix2p.rcf",
+    ATISmallFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --UI fonts
+    Heading1Font = "data:UI\\Font\\MigMix2p.rcf",	-- Title font
+    Heading2Font = "data:UI\\Font\\MigMix2p.rcf",	        -- Sub title font
+    Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Window title font
+	RM_Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Subtitle font
+    Heading4Font = "data:UI\\Font\\MigMix2p.rcf",			-- Window sub title font
+    RM_ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+	ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+    ListBoxItemFont = "data:UI\\Font\\MigMix2p.rcf",
+    GenericSubtitleFont = "data:UI\\Font\\MigMix2p.rcf",
+    ChatFont       = "data:UI\\Font\\MigMix2p.rcf",			-- Font for chat text
+}
+
+FontMap1920 =
+{
+    default = "data:UI\\Font\\MigMix2p.rcf",
+
+	BlenderFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --Subtitle and SP game fonts
+    SPSubtitleFont = "data:UI\\font\\MigMix2p.rcf",
+    LocationCardFont = "data:UI\\Font\\MigMix2p.rcf",			-- Used for mouseover tooltip
+
+    --ATI/SM fonts
+    ATIControlGroupFont = "data:UI\\Font\\MigMix2p.rcf",
+    ATISmallFont = "data:UI\\Font\\MigMix2p.rcf",
+
+    --UI fonts
+    Heading1Font = "data:UI\\Font\\MigMix2p.rcf",	-- Title font
+    Heading2Font = "data:UI\\Font\\MigMix2p.rcf",	        -- Sub title font
+    Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Window title font
+	RM_Heading3Font = "data:UI\\Font\\MigMix2p.rcf",   -- Subtitle font
+    Heading4Font = "data:UI\\Font\\MigMix2p.rcf",			-- Window sub title font
+    RM_ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+	ButtonFont    = "data:UI\\Font\\MigMix2p.rcf", 			-- Font for buttons
+    ListBoxItemFont = "data:UI\\Font\\MigMix2p.rcf",
+    GenericSubtitleFont = "data:UI\\Font\\MigMix2p.rcf",
+    ChatFont       = "data:UI\\Font\\MigMix2p.rcf",			-- Font for chat text
+}
+
